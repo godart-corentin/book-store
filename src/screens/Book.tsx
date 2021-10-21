5a99d2398e6eb4fbe5796d7ba3e08bc48f26d942
Corentin Godart <godartcn@gmail.com>
2021-10-22 00:30:30 +0200

    Mise en place de la navigation

---
+++ src/screens/Book.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {StatusBar, StyleSheet, Text, useColorScheme, View} from 'react-native'
+import {SafeAreaView} from 'react-native-safe-area-context'
+
+export const BookScreen: React.FC<{}> = () => {
+  const isDarkMode = useColorScheme() === 'dark'
+
+  return (
+    <SafeAreaView style={styles.mainView}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={styles.contentView}>
+        <Text style={styles.text}>Book</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+  },
+  contentView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    color: '#212121',
+  },
+})
